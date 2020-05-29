9088b10fd0e3daee4cdc135b7b5a2274811a34e1
YurokBo <vertel3012@mail.ru>
2020-05-29 16:46:36 +0300

    input number

--- app/js/main.js
+++ app/js/main.js
@@ -1,38 +1,54 @@
 $(function () {
-    jQuery('<div class="quantity-nav">' +
-        '<div class="quantity-button quantity-up">+</div>' +
-        '<div class="quantity-button quantity-down">-</div></div>')
-        .insertAfter('.quantity input');
-    jQuery('.quantity').each(function() {
-        var spinner = jQuery(this),
-            input = spinner.find('input[type="number"]'),
-            btnUp = spinner.find('.quantity-up'),
-            btnDown = spinner.find('.quantity-down'),
-            min = input.attr('min'),
-            max = input.attr('max');
 
-        btnUp.click(function() {
-            var oldValue = parseFloat(input.val());
-            if (oldValue >= max) {
-                var newVal = oldValue;
-            } else {
-                var newVal = oldValue + 1;
-            }
-            spinner.find("input").val(newVal);
-            spinner.find("input").trigger("change");
-        });
-
-        btnDown.click(function() {
-            var oldValue = parseFloat(input.val());
-            if (oldValue <= min) {
-                var newVal = oldValue;
-            } else {
-                var newVal = oldValue - 1;
-            }
-            spinner.find("input").val(newVal);
-            spinner.find("input").trigger("change");
-        });
-
+    $('.decrement').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.increment').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
     });
 });
+
+
+/*const input = document.querySelector('input[type=text]');
+
+document.addEventListener('click', e => {
+    // const  up = e.target.closest('.increment');
+        //down = e.target.closest('.decrement');
+    if (!increment(e)) {
+        e.preventDefault()
+    }
+    //decrement(e);
+
+});*/
+
+/*const increment = e => {
+    const up = e.target.closest('.increment');
+    /!*let count = input.value;*!/
+
+    if (!up) {
+        e.preventDefault();
+    }
+    ++input.value;
+    return input.value;
+};*/
+
+/*const decrement = e => {
+   const down = e.target.closest('.decrement');
+   let count =  input.value;
+
+   if (!down) {
+       e.preventDefault();
+   }
+
+    --input.value;
+    return input.value;
+};*/
 
